74dd975cdc3e10b83ad30a54cd50b24d2b638219
DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
2021-03-17 11:49:53 +0100

    ♻️ Add vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue';
-import App from './App.vue';
+import VueRouter from 'vue-router';
+import routes from '@/router';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
 new Vue({
-  render: (h) => h(App),
+  router,
+  template: '<router-view/>',
 }).$mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,8 @@
 import Widget from '@/pages/Widget.vue';
 import HelloWorld from '@/pages/HelloWorld.vue';
 
-const widget = {
-  name: 'widget',
-  component: Widget,
-};
-
-const helloWorld = {
-  name: 'helloWorld',
-  component: HelloWorld,
-};
-
-export default {
-  default: helloWorld,
-  '/helloworld': helloWorld,
-  '/widget': widget,
-};
+export default [
+  { path: '/', component: HelloWorld },
+  { path: '/helloworld', component: HelloWorld },
+  { path: '/widget', component: Widget },
+];
